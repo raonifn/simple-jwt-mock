16aaa6b410318cab7d5b9c18be5d52772980017d
Raoni Normanton <raonifn@gmail.com>
2019-08-13 16:17:57 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const jwt = require('./infra/security/jwt')
+const whitelist = require('./whitelist')
 
 const app = express();
 app.use(bodyParser.json())
+
+const finish = (res, status) => {
+  res.status(status)
+  res.end()
+}
 
 const login = (req, res) => {
   const body = req.body
 
   if (!body || !body.username) {
-    res.status(400)
-    res.end()
-    return
+    return finish(res, 400)
+  }
+  const username = body.username
+  if (!whitelist.includes(username)) {
+    return finish(res, 401)
   }
 
-  const claim = { username: body.username }
+  const claim = { username }
 
   const token = jwt.sign(claim, { expiresIn: '2h' })
   res.send({ token, employeeData: claim })
+  console.log('User logged', claim)
 }
 
 app.post('/login', login)
